21e0dd379f56c6870cf7c0a6affcb4c24264244a
Elliot Chun <eschun@uci.edu>
2025-07-28 22:54:46 -0700

    Update carousel by adding transition out for images

--- carousel.js
+++ carousel.js
@@ -4,9 +4,25 @@ imageCarousels.forEach(carousel => {
 	let currentIndex = 0;
 
 	const showImage = (index) => {
+		let oldImage, newImage;
+
 		images.forEach((image, i) => {
-			image.style.display = i === index ? 'block' : 'none';
+			if (i === index) {
+				image.classList.remove('slide-out');
+				image.classList.add('slide-in');
+				newImage = image;
+			}
+			else {
+				image.classList.remove('slide-in');
+				image.classList.add('slide-out');
+				oldImage = image;
+			}
 		});
+
+		setTimeout(() => {
+			if (oldImage) oldImage.style.display = 'none';
+			if (newImage) newImage.style.display = 'block';
+		}, 500);
 	};
 
 	const nextImage = () => {
