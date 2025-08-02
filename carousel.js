7e3519b593cc4d78ea3d67e0bacab117117a36ca
Elliot Chun <eschun@uci.edu>
2025-08-01 22:16:53 -0700

    Update carousel - set inline style

--- carousel.js
+++ carousel.js
@@ -1,36 +1,37 @@
-const imageCarousels = document.querySelectorAll('.image-carousel');
-imageCarousels.forEach(carousel => {
-	const images = carousel.querySelectorAll('img');
-	let currentIndex = 0;
+// const imageCarousels = document.querySelectorAll('.image-carousel');
+// imageCarousels.forEach(carousel => {
+// 	const images = carousel.querySelectorAll('img');
+// 	let currentIndex = 0;
 
-	const showImage = (index) => {
-		let oldImage, newImage;
+// 	const showImage = (index) => {
+// 		let oldImage, newImage;
 
-		images.forEach((image, i) => {
-			if (i === index) {
-				image.classList.remove('slide-out');
-				image.classList.add('slide-in');
-				newImage = image;
-			}
-			else {
-				image.classList.remove('slide-in');
-				image.classList.add('slide-out');
-				oldImage = image;
-			}
-		});
+// 		images.forEach((image, i) => {
+// 			if (i === index) {
+// 				image.classList.remove('slide-out');
+// 				image.classList.add('slide-in');
+// 				newImage = image;
+// 			}
+// 			else {
+// 				image.classList.remove('slide-in');
+// 				image.classList.add('slide-out');
+// 				oldImage = image;
+// 			}
+// 		});
 
-		setTimeout(() => {
-			if (oldImage) oldImage.style.display = 'none';
-			if (newImage) newImage.style.display = 'block';
-		}, 500);
-	};
+// 		setTimeout(() => {
+// 			if (oldImage) oldImage.style.display = 'none';
+// 			if (newImage) newImage.style.display = 'block';
+// 		}, 500);
+// 	};
 
-	const nextImage = () => {
-		++currentIndex;
-		currentIndex %= images.length;
-		showImage(currentIndex);
-	}
+// 	const nextImage = () => {
+// 		++currentIndex;
+// 		currentIndex %= images.length;
+// 		showImage(currentIndex);
+// 	}
 	
-	showImage(currentIndex);
-	setInterval(nextImage, 5000);
-});
+// 	showImage(currentIndex);
+// 	setInterval(nextImage, 5000);
+// });
+
